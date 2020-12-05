c1db66e51f0a63ebde3041ac614576db5fe3fc03
Laurent Bossavit <laurent.git@bossavit.com>
2020-12-05 13:34:04 +0100

    Day 5 simplified

--- Day05.hs
+++ Day05.hs
@@ -1,18 +1,10 @@
 import Data.List (sort)
 
 seatId :: String -> Int
-seatId passport = (row * 8) + col
-  where row = unbinary $ bdigits ('F','B') $ take 7 passport
-        col = unbinary $ bdigits ('L','R') $ drop 7 passport
+seatId = unbinary . map (fromEnum . flip elem "BR")
 
 unbinary :: [Int] -> Int
 unbinary = foldl (\bit acc -> (bit*2) + acc) 0
-
-bdigits :: (Char, Char) -> String -> [Int]
-bdigits (lo, hi) s = map repl s
-  where repl c | c == lo = 0
-        repl c | c == hi = 1
-        repl _ = error "Only two digits"
 
 findGap :: [Int] -> Int
 findGap l = 1 + head l + (length $ takeWhile (==1) $ steps)
