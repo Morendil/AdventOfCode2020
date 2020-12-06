0c137c4d011752aac96f55f6a77d630a569d7c7e
Laurent Bossavit <laurent.git@bossavit.com>
2020-12-06 11:39:25 +0100

    Refactor day 6

--- Day06.hs
+++ Day06.hs
@@ -1,7 +1,7 @@
 import Text.ParserCombinators.ReadP
 import Data.Char
 import Data.Maybe
-import Data.Set (fromList, size, intersection)
+import Data.Set (fromList, size, intersection, union)
 
 parseMaybe :: ReadP a -> String -> Maybe a
 parseMaybe parser input =
@@ -13,8 +13,12 @@ answers :: ReadP [[String]]
 answers = sepBy1 group (string "\n\n")
   where group = sepBy1 (many1 (satisfy isAlpha)) (string "\n")
 
+solution fn = sum . map (size . foldr1 fn . map fromList) . fromJust
+
 main = do
   contents <- readFile "Day06.txt"
-  let entries = parseMaybe answers contents  
-  putStrLn $ show $ sum $ map size $ map fromList $ map concat $ fromJust entries
-  putStrLn $ show $ sum $ map (size . foldr1 intersection . map fromList) $ fromJust entries
+  let entries = parseMaybe answers contents
+  -- part1
+  putStrLn $ show $ solution union entries
+  -- part2
+  putStrLn $ show $ solution intersection entries
