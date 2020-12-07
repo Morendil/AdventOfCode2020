561453226a857b664dc8b9d526cda8201671f007
Laurent Bossavit <laurent.git@bossavit.com>
2020-12-07 10:11:49 +0100

    Day 7 part 1

---
+++ Day07.hs
@@ -0,0 +1,60 @@
+import Text.ParserCombinators.ReadP
+import Data.Char
+import Data.Maybe
+import Data.Graph
+import Data.List
+
+type Bag = (String, [(Int, String)])
+
+main = do
+  contents <- readFile "Day07.txt"
+  let entries = fromJust $ parseMaybe bags contents
+  let edgeList = map edge entries
+  let (graph, nodeFromVertex, vertexFromKey) = graphFromEdges edgeList
+  let pathToSg (desc,_) = path graph (fromJust $ vertexFromKey desc) (fromJust $ vertexFromKey "shinygold")
+  putStrLn $ show $ (length $ filter pathToSg entries) - 1
+
+color :: ((), String, [String]) -> String
+color (_, desc, inner) = desc
+
+edge :: Bag -> ((), String, [String])
+edge (desc, inner) = ((), desc, map snd inner)
+
+parseMaybe :: ReadP a -> String -> Maybe a
+parseMaybe parser input =
+    case reverse $ readP_to_S parser input of
+        [] -> Nothing
+        ((result, _):_) -> Just result
+
+description :: ReadP String
+description = do
+  adjective <- many1 (satisfy isAlpha)
+  char ' '
+  color <- many1 (satisfy isAlpha)
+  return $ adjective ++ color
+
+inner :: ReadP (Int, String)
+inner = do
+  count <- many1 (satisfy isNumber)
+  char ' '
+  desc <- description
+  string " bag"
+  optional (char 's')
+  return (read count, desc)
+
+bag :: ReadP Bag
+bag = do
+  desc <- description
+  inside <- choice [empty, nonEmpty]
+  return (desc, inside)
+  where empty = do
+          string " bags contain no other bags."
+          return []    
+        nonEmpty = do
+          string " bags contain "
+          list <- sepBy1 inner (string ", ")
+          string "."
+          return list
+
+bags :: ReadP [Bag]
+bags = sepBy1 bag (char '\n')
