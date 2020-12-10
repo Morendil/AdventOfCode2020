232ac2e5e4f8003aa9c14d292e172157bd5bc67d
Laurent Bossavit <laurent.git@bossavit.com>
2020-12-10 15:39:19 +0100

    Day 10 redux

--- Day10.hs
+++ Day10.hs
@@ -1,17 +1,19 @@
 import Data.List
+import Data.Maybe
+import qualified Data.Map as M
 
 main = do
-  contents <- readFile "Day10.txt"
+  contents <- readFile "sample1.txt"
   let entries = map (read :: String -> Integer) $ lines contents
   let sorted = sort entries
   let differences = (zipWith (-) sorted (0:sorted)) ++ [3]
   let count n l = length $ filter (==n) l
   -- part 1
   print $ (count 1 differences) * (count 3 differences)
-  print $ product $ map (\n -> toInteger . length $ arrangements [1..n+1]) $ map (toInteger .length) $ filter (\l -> head l == 1 && length l > 1) $ group differences
+  -- part 2
+  print sorted
+  print $ countArrangements sorted
 
-arrangements :: [Integer] -> [[Integer]]
-arrangements [x] = [[x]]
-arrangements (x:xs) = map (\path -> x:path) $ concatMap arrangements paths
-  where nexts = takeWhile (<=x+3) xs
-        paths = map (flip drop xs) [0..length nexts-1]
+countArrangements :: [Integer] -> Maybe Integer
+countArrangements levels = M.lookup (last levels) $ foldl step (M.fromList [(0,1)]) levels
+  where step state level = M.insert level (sum $ mapMaybe (flip M.lookup state) [level-1,level-2,level-3]) state
