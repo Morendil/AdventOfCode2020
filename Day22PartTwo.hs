7ebe25bf4df7f0cf2aaf72cdf7db245b458106f8
Laurent Bossavit <laurent.git@bossavit.com>
2020-12-22 17:34:48 +0100

    Make faster

--- Day22PartTwo.hs
+++ Day22PartTwo.hs
@@ -1,21 +1,22 @@
 import Data.Text (splitOn, pack, unpack)
-import Debug.Trace
+import qualified Data.Set as S
 
 type Game = [[Int]]
-type Games = [Game]
+type Games = S.Set Game
 type State = (Games, Game)
 
 play :: State -> State
 play (s, [[],p2]) = (s, [[],p2])
 play (s, [p1,[]]) = (s, [p1,[]])
-play (s, g@([p1,p2])) | g `elem` s = ([],[p1,[]])
+play (s, g@([p1,p2])) | g `S.member` s = (S.empty,[p1,[]])
 play (s, g@[c1:r1,c2:r2]) = if ok then recurse else normal
   where ok = length r1 >= c1 && length r2 >= c2
         recurse = if null p2r then p1win else p2win
-        (_, [p1r,p2r]) = result ([], [take c1 r1, take c2 r2])
+        (_, [p1r,p2r]) = result (S.empty, [take c1 r1, take c2 r2])
         normal = if c1 > c2 then p1win else p2win
-        p1win = let e = [r1++[c1,c2],r2] in (g:s,e)
-        p2win = let e = [r1,r2++[c2,c1]] in (g:s,e)
+        p1win = let e = [r1++[c1,c2],r2] in (ns,e)
+        p2win = let e = [r1,r2++[c2,c1]] in (ns,e)
+        ns = S.insert g s
 
 result :: State -> State
 result s = play $ last $ fullGame
@@ -35,4 +36,4 @@ main = do
       split = map (tail . splitOn (pack "\n")) raw
       decks :: [[Int]]
       decks = map (map (read . unpack)) split
-  print $ score $ snd $ result ([], decks)
+  print $ score $ snd $ result (S.empty, decks)
