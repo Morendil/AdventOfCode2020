a4d985da917c26f2ed3f1ef34eb706641f511d00
Laurent Bossavit <laurent.git@bossavit.com>
2020-12-22 11:52:20 +0100

    Day 22 part two (fixed)

--- Day22PartTwo.hs
+++ Day22PartTwo.hs
@@ -11,12 +11,15 @@ play (s, [p1,[]]) = (s, [p1,[]])
 play (s, g@([p1,p2])) | g `elem` s = ([],[p1,[]])
 play (s, g@[c1:r1,c2:r2]) = if ok then recurse else normal
   where ok = length r1 >= c1 && length r2 >= c2
-        recurse = if rec1win then p1win else p2win
-        rec1win = null p2r
-        (_, [p1r,p2r]) = play ([], [take c1 r1, take c2 r2])
+        recurse = if null p2r then p1win else p2win
+        (_, [p1r,p2r]) = result ([], [take c1 r1, take c2 r2])
         normal = if c1 > c2 then p1win else p2win
         p1win = let e = [r1++[c1,c2],r2] in (g:s,e)
         p2win = let e = [r1,r2++[c2,c1]] in (g:s,e)
+
+result :: State -> State
+result s = play $ last $ fullGame
+  where fullGame = takeWhile (not.done) $ iterate play s
 
 score :: Game -> Int
 score = sum . mark . head . filter (not.null)
@@ -27,9 +30,9 @@ done :: State -> Bool
 done (s,[p1,p2]) = null p1 || null p2
 
 main = do
-  contents <- readFile "sample.txt"
+  contents <- readFile "Day22.txt"
   let raw = splitOn (pack "\n\n") (pack $ contents)
       split = map (tail . splitOn (pack "\n")) raw
       decks :: [[Int]]
       decks = map (map (read . unpack)) split
-  print $ score $ snd $ play $ last $ takeWhile (not.done) $ iterate play ([], decks)
+  print $ score $ snd $ result ([], decks)
